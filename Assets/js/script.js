f6d2fa35299b565f483aaac75a87299b6bf95fcb
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-05-16 21:25:36 -0600

    began a little bit of the fetch api function. just added my api id to the link that I need to use

---
+++ Assets/js/script.js
@@ -0,0 +1,3 @@
+// CURRENT AND FORECAST WEATHER DATA
+var currentWeatherURL =
+  "https://api.openweathermap.org/data/2.5/onecall?lat={GET LAT VAR}&lon{GET LON VAR}&exclude=minutely,hourly,daily,alerts&appid=035e64d4cce284ef774e18d498588393";
