26d2efef9e4821b99d1f3f54ea7766fce02c76bd
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-05-18 20:33:57 -0600

    added some psuedo code to help map out what I am trying to code, and added moment.js feature to the current day weather

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,19 @@
+// MOMENT JS VARIABLES
+var currentDay = moment();
+$("#current-date").text(currentDay.format("MMMM Do, YYYY"));
+
 // CURRENT AND FORECAST WEATHER DATA
 var currentWeatherURL =
   "https://api.openweathermap.org/data/2.5/onecall?lat={GET LAT VAR}&lon{GET LON VAR}&exclude=minutely,hourly,daily,alerts&appid=035e64d4cce284ef774e18d498588393";
+
+// USER INPUT VARIABLES
+var userCity = document.getElementById("city-search");
+
+// SEARCH BUTTON VARIABLE
+var searchBtn = document.getElementById("search-button");
+
+// SEARCH BUTTON EVENT LISTENER
+
+searchBtn.addEventListener("click", function () {
+  localStorage.clear();
+});
